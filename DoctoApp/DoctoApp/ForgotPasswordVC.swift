c257061e294e7cc75d7740a83b47ae30c8330c66
COPPENS EWEN <coppense@mac03-m102.utt.fr>
2019-12-09 20:04:51 +0100

    [WIP] Forgot password storyboard

---
+++ DoctoApp/DoctoApp/ForgotPasswordVC.swift
@@ -0,0 +1,30 @@
+//
+//  ForgotPasswordVC.swift
+//  DoctoApp
+//
+//  Created by COPPENS EWEN on 09/12/2019.
+//  Copyright © 2019 DoctoAppLavCop. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
