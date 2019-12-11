cec41a5fdb391d48ecc69148a0e95274a07a0f93
COPPENS EWEN <coppense@mac10-m102.utt.fr>
2019-12-11 20:33:29 +0100

    [WIP] Delete account popup

---
+++ DoctoApp/DoctoApp/LoginPopUpDeleteAccountVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginPopUpDeleteAccountVC.swift
+//  DoctoApp
+//
+//  Created by COPPENS EWEN on 11/12/2019.
+//  Copyright © 2019 DoctoAppLavCop. All rights reserved.
+//
+
+import UIKit
+
+class LoginPopUpDeleteAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissPopUp(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
