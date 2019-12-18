f247fed54ac5d0ab43e435bb9a612adb46e51517
COPPENS EWEN <coppense@mac03-m102.utt.fr>
2019-12-18 22:11:45 +0100

    Completed LoginVC and [WIP] SignUpProVC.

--- DoctoApp/DoctoApp/Controllers/LoginVC.swift
+++ DoctoApp/DoctoApp/Controllers/LoginVC.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class LoginVC: UIViewController {
+    
+    private var loggedUser: Resident?
+    private var toRedirect: Bool!
+    
     @IBOutlet weak var loginMsg: UIView!
+    @IBOutlet weak var loginMsgTitle: UILabel!
+    @IBOutlet weak var loginMsgContent: UILabel!
     @IBOutlet weak var deleteAccountMsg: UIView!
     @IBOutlet weak var emailInput: UITextField!
     @IBOutlet weak var passwordInput: UITextField!
@@ -19,15 +25,156 @@ class LoginVC: UIViewController {
     @IBOutlet weak var logoutBtn: UIButton!
     @IBOutlet weak var optionsSection: UIStackView!
     @IBOutlet weak var signupSection: UIView!
+    @IBOutlet weak var deleteMyAccountBtn: UIButton!
     @IBOutlet weak var professionalSection: UIView!
     @IBOutlet weak var myProfileBtn: UIButton!
     @IBOutlet weak var myBookingsBtn: UIButton!
     
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.setContent()
+    }
+    
+    private func setContent() -> Void {
+        self.setLoginContext()
+        
+        if (self.loggedUser != nil) {
+            self.displaySuccessMsg()
+        }
         
     }
     
+    private func setLoginContext() -> Void {
+        emailInput.isHidden = false
+        passwordInput.isHidden = false
+        optionsSection.isHidden = false
+        loginBtn.isHidden = false
+        myProfileBtn.isHidden = true
+        myBookingsBtn.isHidden = true
+        deleteMyAccountBtn.isHidden = true
+        logoutBtn.isHidden = true
+        signupSection.isHidden = false
+        professionalSection.isHidden = false
+        loginMsg.isHidden = true
+        deleteAccountMsg.isHidden = true
+        emailInput.text = ""
+        passwordInput.text  = ""
+        stayLogged.setOn(false, animated: false)
+        
+    }
+    
+    
+    @IBAction func login(_ sender: Any) {
+        //self.checkIfRedirect
+        var success: Bool = self.tryLoginAsPatient()
+        if (!self.toRedirect) {
+            if (!success) {
+                success = self.tryLoginAsDoctor()
+            }
+        }
+        if (!success) {
+            self.displayErrorMsg()
+        }
+        
+    }
+    
+    private func tryLoginAsPatient() -> Bool {
+        let inputEmail: String = emailInput.text!
+        
+        // Try to get a patient using the provided email
+        let patient: Patient? = PatientDatabaseHelper().getPatient(patientId: nil, email: inputEmail, fromDoctor: false)
+        
+        // If the email isn't matched
+        if (patient == nil) {
+            return false
+        }
+        
+        //Check the password
+        let inputPwd = passwordInput.text!
+        let salt = patient?.getPwdSalt()
+        let hashedInputPwd = inputPwd + salt! //TO DO: hash it with SHA1
+        let patientPwd = patient?.getPwd()
+        
+        // If the password isn't matched
+        if ((patientPwd?.elementsEqual(hashedInputPwd))!) {
+            return false
+        }
+        
+        self.displaySuccessMsg()
+        self.loggedUser = patient
+        
+        if (self.toRedirect) {
+             //REDIRECT
+        }
+        
+        return true
+    }
+    
+    private func tryLoginAsDoctor() -> Bool {
+        let inputEmail: String = emailInput.text!
+        
+        // Try to get a patient using the provided email
+        let doctor: Doctor? = DoctorDatabaseHelper().getDoctor(doctorId: nil, email: inputEmail, fromPatient: false)
+        
+        // If the email isn't matched
+        if (doctor == nil) {
+            return false
+        }
+        
+        //Check the password
+        let inputPwd = passwordInput.text!
+        let salt = doctor?.getPwdSalt()
+        let hashedInputPwd = inputPwd + salt! //TO DO: hash it with SHA1
+        let patientPwd = doctor?.getPwd()
+        
+        // If the password isn't matched
+        if ((patientPwd?.elementsEqual(hashedInputPwd))!) {
+            return false
+        }
+        
+        self.displaySuccessMsg()
+        self.loggedUser = doctor
+        
+        if (self.toRedirect) {
+            //REDIRECT
+        }
+        
+        return true
+    }
+    
+    private func displaySuccessMsg() -> Void {
+        loginMsg.isHidden = false
+        loginMsg.backgroundColor = UIColor(hex: Colors.LOGIN_SUCCESS_MSG)
+        loginMsgTitle.text = Strings.LOGIN_SUCCESS_MSG_TITLE
+        loginMsgContent.text = Strings.LOGIN_SUCCESS_MSG_CONTENT
+        deleteAccountMsg.isHidden = true
+        emailInput.isHidden = true
+        passwordInput.isHidden = true
+        optionsSection.isHidden = true
+        loginBtn.isHidden = true
+        myProfileBtn.isHidden = false
+        myBookingsBtn.isHidden = false
+        deleteMyAccountBtn.isHidden = false
+        logoutBtn.isHidden = false
+        signupSection.isHidden = true
+        professionalSection.isHidden = true
+    }
+    
+    private func displayErrorMsg() -> Void {
+        loginMsg.isHidden = false
+        loginMsg.backgroundColor = UIColor(hex: Colors.LOGIN_ERROR_MSG)
+        loginMsgTitle.text = Strings.LOGIN_ERROR_MSG_TITLE
+        loginMsgContent.text = Strings.LOGIN_ERROR_MSG_CONTENT
+    }
+    
+    @IBAction func logout(_ sender: Any) {
+        self.loggedUser = nil
+        self.setLoginContext()
+    }
 
+    
+    
 }
