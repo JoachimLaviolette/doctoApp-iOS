4ab78b07a6523dbbcec2baaca61cdfcce55448b2
Joachim Laviolette <joachim.laviolette@gmail.com>
2019-12-21 21:18:22 +0100

    Prepared EncryptionService methods.

--- DoctoApp/DoctoApp/Services/EncryptionService.swift
+++ DoctoApp/DoctoApp/Services/EncryptionService.swift
@@ -1,1 +1,17 @@
+import Foundation
 
+class EncryptionService {
+    // SHA1 the given string
+    static SHA1(string: String) -> String {
+        var encryptedString: String = string
+        
+
+
+        return encryptedString
+    }
+
+    // Create a unique random and ecrypted salt
+    static func SALT() {
+        return EncryptionService.SHA1(string: UUID().uuidString)
+    }
+}
